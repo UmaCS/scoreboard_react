f5b76142ece339c896dc3c9038e4f401296e5ccf
UmaCS <wiutumka@gmail.com>
2020-01-19 16:36:32 -0800

    first

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React,{Component} from 'react';
+import './App.css';
+
+import Header from './Components/Header';
+import Player from './Components/Player';
+
+class App extends Component {
+  state = {
+    players:[
+      {
+        name: "Umid",
+        score: 0,
+        id: 1
+      },
+      {
+        name: "Guil",
+        score: 0,
+        id: 2
+      },
+      {
+        name: "Ashley",
+        score: 0,
+        id: 3
+      },
+      {
+        name: "James",
+        score: 0,
+        id: 4
+      }
+    ]
+  }
+
+  handleScoreChange = (index,delta) => {
+    this.setState(prevState => ({
+      score: prevState.players[index].score += delta
+    }));
+  }
+
+  handleRemovePlayer = (id) => {
+    this.setState(prevState => {
+      return {
+        players:prevState.players.filter(p => p.id !== id)
+      }
+    })
+  }
+
+  render(){
+    return (
+      <div className='scoreboard'>
+        <Header title='Scoreboard' totalPlayers={this.state.players.length} />
+        {/* Players List */}
+        {this.state.players.map( (player,index) =>
+          <Player
+          name={player.name}
+          score={player.score}
+          index={index}
+          id={player.id}
+          key={player.id.toString()}
+          removePlayer={this.handleRemovePlayer}
+          changeScore={this.handleScoreChange}
+           />
+        )}
+      </div>
+    );
+  }
+
+}
+
+
+export default App;
